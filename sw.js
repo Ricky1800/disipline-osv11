7beb42753972d949e85d8e862e1aa0fb2a096ccf
Ricky1800 <121143195+Ricky1800@users.noreply.github.com>
2026-01-07 18:26:48 -0500

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('install',e=>{self.skipWaiting()});self.addEventListener('fetch',e=>{});
+const CACHE="dos-flat-v1";
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      "./",
+      "./index.html",
+      "./styles.css",
+      "./app.js"
+    ]))
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
